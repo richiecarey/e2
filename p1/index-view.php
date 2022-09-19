59901f11186a4be392d881aa21fdd2d646cae198
Richie Carey <richiecarey@gmail.com>
2022-09-19 04:54:55 +0000

    Progress week 3

---
+++ p1/index-view.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang='en'>
+<head>
+    <title><?php echo $title ?></title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+</head>
+<body>
+    <h1>Project 1</h1>
+    <h2>Mechanics</h2>
+        <ul>
+            <li>...</li>
+        </ul>
+    <h2>Results</h2>
+        <ul>
+            <li>...</li>
+        </ul>
+</body>
+</html>
