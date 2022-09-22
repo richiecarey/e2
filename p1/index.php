0c880e7db61b2ce5e87ed8916c2884fc3f223cda
Richie Carey <richiecarey@gmail.com>
2022-09-22 17:13:46 +0000

    Change variable name.

--- p1/index.php
+++ p1/index.php
@@ -3,12 +3,12 @@
 $title = 'Project 1';
 
 # Construct a standard 52 card deck of cards
-$composition = [
+$standard = [
     'suit' => ['♠','♣','♦','♥'],
     'rank' => ['A','2','3','4','5','6','7','8','9','10','J','Q','K']    
 ];
-foreach($composition['suit'] as $suit) {
-    foreach($composition['rank'] as $rank) {
+foreach($standard['suit'] as $suit) {
+    foreach($standard['rank'] as $rank) {
         $deck[] = [
             'rank' => $rank,
             'suit' => $suit
