4de9db77bbe1914a7276215391b40339f81afebc
Richie Carey <richiecarey@gmail.com>
2022-09-22 15:43:34 +0000

    Create arrays and loops to construct a card deck.

--- p1/index-view.php
+++ p1/index-view.php
@@ -4,9 +4,15 @@
     <title><?php echo $title ?></title>
     <meta charset='utf-8'>
     <link href=data:, rel=icon>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <h1>Project 1</h1>
+    <section class="border-2 border-indigo-600 mb-8 mt-8 p-4">
+        <pre>
+<?php var_dump($hand) ?>
+        </pre>
+    </section>
     <h2>Mechanics</h2>
         <ul>
             <li>...</li>
--- p1/index.php
+++ p1/index.php
@@ -2,4 +2,18 @@
 
 $title = 'Project 1';
 
+# https://en.wikipedia.org/wiki/Playing_cards_in_Unicode
+$deck = [
+    'suit' => ['♠','♣','♦','♥'],
+    'rank' => ['A','2','3','4','5','6','7','8','9','10','J','Q','K']    
+];
+
+foreach($deck['suit'] as $suit) {
+    foreach($deck['rank'] as $rank) {
+        $hand[] = $rank.$suit;
+    }
+}
+
+//shuffle($hand);
+
 require 'index-view.php';
