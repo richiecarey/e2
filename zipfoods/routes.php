2f85d403a397730445d4c8fb1ec91f9bec6f4a41
Richie Carey <richiecarey@gmail.com>
2022-11-25 21:00:17 +0000

    week 12 and week 13 updates.

--- zipfoods/routes.php
+++ zipfoods/routes.php
@@ -5,4 +5,6 @@
 return [
     # Ex: The path `/` will trigger the `index` method within the `AppController`
     '/' => ['AppController', 'index'],
+    '/practice' => ['AppController', 'practice'],
+    '/products' => ['ProductsController', 'index'],
 ];
--- zipfoods/views/index.blade.php
+++ zipfoods/views/index.blade.php
@@ -1,14 +1,14 @@
 @extends('templates/master')
 
 @section('title')
-    {{ $welcome }}
+{{ $welcome }}
 @endsection
 
 @section('content')
 
-    <h2>{{ $welcome }}</h2>
+<h2>{{ $welcome }}</h2>
 
-    <p>Hello and welcome! This is the boilerplate splash page for my application built with <a
-            href='https://github.com/susanBuck/e2framework'>e2framework</a>.</p>
+<p>Hello and welcome! This is the boilerplate splash page for my application built with <a
+        href='https://github.com/susanBuck/e2framework'>e2framework</a>.</p>
 
 @endsection
