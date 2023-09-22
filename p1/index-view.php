d49f7c75e56631969d96119af6fe0bb0ee0b6542
Richie Carey <richiecarey@gmail.com>
2023-09-22 14:16:34 +0000

    Separate p1 into controller and view files

---
+++ p1/index-view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project 1</title>
+</head>
+
+<body>
+    <h1>Projects 1</h1>
+    <h2>Game Mechanics</h2>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+    <h2>Results</h2>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</body>
+
+</html>
--- p1/index.php
+++ p1/index.php
@@ -1,26 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project 1</title>
-</head>
-
-<body>
-    <h1>Projects 1</h1>
-    <h2>Game Mechanics</h2>
-    <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
-    <h2>Results</h2>
-    <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
-</body>
-
-</html>
+require 'index-view.php';
